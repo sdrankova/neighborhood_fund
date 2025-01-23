8958b4d8efa965d7c9969d266f4e0bedc625e872
sdrankova <sanny9676@gmail.com>
2025-01-23 11:28:02 +0200

    Add Neighbor and Contribution models to contributions app

--- contributions/models.py
+++ contributions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Neighbor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Contribution(models.Model):
+    neighbor = models.ForeignKey(Neighbor, on_delete=models.CASCADE, related_name='contributions')
+    date = models.DateField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.neighbor.name} - {self.amount} on {self.date}'
