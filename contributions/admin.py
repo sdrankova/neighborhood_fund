c162183f88cf4534de77cb5cc5793b467d55dbb2
sdrankova <sanny9676@gmail.com>
2025-01-23 11:38:54 +0200

    Register Neighbor and Contribution models in admin

--- contributions/admin.py
+++ contributions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contribution, Neighbor
 
-# Register your models here.
+
+@admin.register(Neighbor)
+class NeighborAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+
+
+@admin.register(Contribution)
+class ContributionAdmin(admin.ModelAdmin):
+    list_display = ['neighbor', 'amount', 'date']
+    list_filter = ['date']
